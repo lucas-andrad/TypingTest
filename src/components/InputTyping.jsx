509a2799849f50a0c04d39178070841ea9736334
unknown <lafdesouza2002@outlook.com>
2020-12-27 11:35:24 -0300

    Fixing texarea and ajusting some files

--- src/components/InputTyping.jsx
+++ src/components/InputTyping.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './InputTyping.css';
 
 export default function InputTyping() {
-    const [typed, setTyped] = useState('')
-    function verifyLetters(target) {
-        setTyped(target.value)
-    }
-    return (
-        <textarea name="" id="" cols="30" rows="10" onChange={(target) => verifyLetters(target)} value={typed}></textarea>
-    )
+  const [typed, setTyped] = useState('');
+  function verifyLetters(target) {
+    setTyped(target.value);
+  }
+  return (
+    <textarea
+      name=""
+      id=""
+      cols="100"
+      rows="20"
+      onChange={(target) => verifyLetters(target)}
+      value={typed}
+    ></textarea>
+  );
 }
--- src/components/WordsToType.jsx
+++ src/components/WordsToType.jsx
@@ -1,27 +1,31 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import Loading from './Loading';
-import './WordsToType.css'
+import './WordsToType.css';
 
 export default function WordsToType() {
-    const [ words, setWords ] = useState([])
-    const [ loaded, setLoaded ] = useState(false)
+  const [words, setWords] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-    
+  useEffect(() => {
+    fetchWords();
+  }, []);
 
-    const fetchWords = async () => {
-        setLoaded(false)
-        const response = await fetch('https://random-word-api.herokuapp.com/word?number=30')
-        const data = await response.json()
-        setWords(data)
-        setLoaded(true)
-    }
+  const fetchWords = async () => {
+    setLoaded(false);
+    const response = await fetch(
+      'https://random-word-api.herokuapp.com/word?number=30'
+    );
+    const data = await response.json();
+    setWords(data);
+    setLoaded(true);
+  };
 
-    return (
-        <div className='words'>
-            <div>
-                { !loaded ? <Loading /> : words.map(word => <span>{word} </span>) }
-            </div>
-            <button onClick={() => fetchWords()}>Fetch</button>
-        </div>
-    )
+  return (
+    <div className="words">
+      <div>
+        {!loaded ? <Loading /> : words.map((word) => <span>{word} </span>)}
+      </div>
+      <button onClick={() => fetchWords()}>Fetch</button>
+    </div>
+  );
 }
